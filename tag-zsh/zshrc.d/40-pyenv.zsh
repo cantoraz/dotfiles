56dd361e69478a64c92de68a8cc0dca348e21855
Cantoraz Chou <cantoraz@gmail.com>
2022-01-18 16:32:59 +0800

    feat: set up Zsh autocompletion for `pyenv' installed by package manager

---
+++ tag-zsh/zshrc.d/40-pyenv.zsh
@@ -0,0 +1,7 @@
+# -*- sh-basic-offset: 2; -*-
+# vim: ft=zsh ts=2 sts=2 sw=2 et
+
+# Load pyenv
+if [[ $commands[pyenv] == '/usr/bin/pyenv' ]]; then
+  source '/usr/share/zsh/site-functions/_pyenv'
+fi
