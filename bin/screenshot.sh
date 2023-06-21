7d0d953672a75b04a28a1f60a64de14f6df4b166
Cantoraz Chou <cantoraz@gmail.com>
2023-06-21 14:05:11 +0800

    refactor: update bin/screenshot.sh

--- bin/screenshot.sh
+++ bin/screenshot.sh
@@ -12,7 +12,7 @@ fi
 
 cd "$(dirname "$0")"
 
-declare     SHOT_DIR=$(xdg-user-dir PICTURES)
+declare    SHOT_DIR=$(xdg-user-dir PICTURES)
 if [ $(xdg-user-dir) = $SHOT_DIR ]; then
   SHOT_DIR="~/Pictures"
 fi
@@ -21,17 +21,17 @@ if ! [ -d $SHOT_DIR ]; then
   mkdir -p $SHOT_DIR
 fi
 
-declare -r  SHOT_EXT=png
-declare -r  SHOT_FNAME="Screenshot_$(date +%Y%m%d_%H%M%S).$SHOT_EXT"
-declare -r  SHOT_PATH="$SHOT_DIR/$SHOT_FNAME"
-declare -r  SHOT_TMP=$(mktemp -q)
-declare     SHOT_MODE=
-declare     SHOT_COPY=0
+declare -r SHOT_EXT=png
+declare -r SHOT_FNAME="Screenshot_$(date +%Y%m%d_%H%M%S).$SHOT_EXT"
+declare -r SHOT_PATH="$SHOT_DIR/$SHOT_FNAME"
+declare -r SHOT_TMP=$(mktemp -q)
+declare    SHOT_MODE=
+declare    SHOT_COPY=0
 
-declare -r  NOTIFY_APP_NAME=screenshot
-declare     NOTIFY_CATEGORY=string:category:screenshot
-declare     NOTIFY_SUMMARY=
-declare     NOTIFY_BODY="A screenshot was saved as '$SHOT_FNAME' to your ${SHOT_DIR/#$HOME/\~} folder."
+declare -r NOTIFY_APP_NAME=screenshot
+declare    NOTIFY_CATEGORY=string:category:screenshot
+declare    NOTIFY_SUMMARY=
+declare    NOTIFY_BODY="A screenshot was saved as '$SHOT_FNAME' to your ${SHOT_DIR/#$HOME/\~} folder."
 
 print_usage() {
   echo "Usage: $(basename $0) [options]
@@ -56,12 +56,22 @@ add_shadow() {
 capture() {
   add_shadow "$SHOT_TMP"
   if [ $SHOT_COPY = 0 ]; then
-    # add_shadow $SHOT_PATH
     cp "$SHOT_TMP" "$SHOT_PATH"
+    chmod $(printf '%03o' $((8#666-8#$(umask)))) "$SHOT_PATH"
   else
     # add_shadow - | xclip -sel clip -t image/png
     xclip -sel clip -t image/png "$SHOT_TMP"
   fi
+}
+
+notify() {
+  local notify_options="-a '$NOTIFY_APP_NAME' -h $NOTIFY_CATEGORY"
+  [ ! -v ERR ] && notify_options+=" -I $SHOT_TMP"
+  dunstify $notify_options "$NOTIFY_SUMMARY" "$NOTIFY_BODY"
+}
+
+cleanup() {
+  [ -O "$SHOT_TMP" ] && rm "$SHOT_TMP"
 }
 
 main() {
@@ -69,40 +79,31 @@ main() {
     screen|'')
       NOTIFY_SUMMARY="Current Screen"
       shotgun -s - | capture
-      # ksnip -m
       ;;
     fullscreen)
       NOTIFY_SUMMARY="Fullscreen"
       shotgun - | capture
-      # ksnip -f
       ;;
     region)
       NOTIFY_SUMMARY="Rectangular Region"
       local -r sel=$(slop -b 1 -c 0.239,0.682,0.914,0.4 -f '-i %i -g %g' -l -q)
       if [ -z "$sel" ]; then
-        NOTIFY_CATEGORY=$NOTIFY_CATEGORY.abort
+        declare -r ERR=1
+        NOTIFY_CATEGORY+=.abort
         NOTIFY_BODY="Screenshot aborted."
-        local -r ret=1
       else
         shotgun $sel - | capture
-        # ksnip -r
       fi
       ;;
     window)
       NOTIFY_SUMMARY="Active Window"
       shotgun -i $(xdotool getactivewindow) - | capture
-      # ksnip -a
       ;;
   esac
 
-  if [ -v ret ]; then
-    dunstify -a "$NOTIFY_APP_NAME" -h "$NOTIFY_CATEGORY" "$NOTIFY_SUMMARY" "$NOTIFY_BODY"
-  else
-    dunstify -a "$NOTIFY_APP_NAME" -h "$NOTIFY_CATEGORY" -I "$SHOT_TMP" "$NOTIFY_SUMMARY" "$NOTIFY_BODY"
-  fi
-
-  [ -O "$SHOT_TMP" ] && rm "$SHOT_TMP"
-  exit ${ret:-0}
+  notify
+  cleanup
+  exit ${ERR:-0}
 }
 
 while getopts ":frswch" options; do
