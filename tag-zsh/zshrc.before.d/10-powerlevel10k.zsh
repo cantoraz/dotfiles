157888d63cf36190b29757d5e25cf74cbc344aae
Cantoraz Chou <cantoraz@gmail.com>
2024-03-11 19:25:27 +0800

    feat(zsh): configure powerlevel10k

---
+++ tag-zsh/zshrc.before.d/10-powerlevel10k.zsh
@@ -0,0 +1,8 @@
+# vim: ft=zsh ts=2 sts=2 sw=2 et
+
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
