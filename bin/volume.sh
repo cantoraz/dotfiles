3bf8bfe3817a67d266c4ccc282895c507bf5f397
Cantoraz Chou <cantoraz@gmail.com>
2025-08-14 15:34:22 +0800

    fix: use `pactl` in lieu of `pulseaudio-ctl` in script `bin/volume.sh` to control volume due to removal of the latter from Arch repository

--- bin/volume.sh
+++ bin/volume.sh
@@ -29,16 +29,19 @@ main() {
     local -r IS_MUTE=*' 'yes
     case $1 in
         mute)
-            pulseaudio-ctl mute
+            # pulseaudio-ctl mute
+            pactl set-sink-mute $SINK toggle
             [[ $OLD_MUTE == $IS_MUTE ]] && canberra-gtk-play -i $SOUND || exit 0
             ;;
         up)
-            pulseaudio-ctl up $STEP
+            # pulseaudio-ctl up $STEP
+            pactl set-sink-volume $SINK +$STEP%
             [[ $OLD_MUTE == $IS_MUTE ]] && pactl set-sink-mute $SINK no
             canberra-gtk-play -i $SOUND
             ;;
         down)
-            pulseaudio-ctl down $STEP
+            # pulseaudio-ctl down $STEP
+            pactl set-sink-volume $SINK -$STEP%
             [[ $OLD_MUTE == $IS_MUTE ]] && pactl set-sink-mute $SINK no
             canberra-gtk-play -i $SOUND
             ;;
