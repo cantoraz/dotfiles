6063745d45ea3b0e5a105c8d8c111a71684fbf6d
Cantoraz Chou <cantoraz@gmail.com>
2026-01-27 18:10:04 +0800

    feat(lazyvim): change colorscheme to `catppuccin`

---
+++ tag-lazyvim/config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "catppuccin/nvim",
+    opts = {
+      -- transparent_background = true, -- disables setting the background color.
+      float = {
+        transparent = true, -- enable transparent floating windows
+      },
+    },
+  },
+}
