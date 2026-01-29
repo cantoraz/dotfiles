1c77dc7af31ed891e43fa9b829087db9866f8015
Cantoraz Chou <cantoraz@gmail.com>
2026-01-29 10:32:44 +0800

    feat(lazyvim): enable extra `util.dot`
    - prevent Mason from installing `bashls`

--- tag-lazyvim/config/nvim/lua/plugins/lsp.lua
+++ tag-lazyvim/config/nvim/lua/plugins/lsp.lua
@@ -2,11 +2,21 @@ return {
   {
     "mason-org/mason.nvim",
     opts = function(_, opts)
-      local preinstalled = { "stylua", "shfmt" }
+      local preinstalled = { "stylua", "shfmt", "shellcheck" }
       local function missing(package)
         return not vim.tbl_contains(preinstalled, package)
       end
       opts.ensure_installed = vim.tbl_filter(missing, opts.ensure_installed)
     end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- required by extra `util.dot`, but provided by system rather than Mason
+        bashls = { mason = false },
+      },
+    },
   },
 }
