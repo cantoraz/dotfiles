a5e07f685d1b45ffe3822e5d68359a55d73f0671
Cantoraz Chou <cantoraz@gmail.com>
2026-01-30 19:32:10 +0800

    feat(lazyvim): enable extra `lang.go`
    - prevent Mason from install `goimports`, `gomodifytags`, `golangci-lint`
    - prevent Mason from installing DAP adapter (`delve`) for Go

--- tag-lazyvim/config/nvim/lua/plugins/lsp.lua
+++ tag-lazyvim/config/nvim/lua/plugins/lsp.lua
@@ -2,7 +2,22 @@ return {
   {
     "mason-org/mason.nvim",
     opts = function(_, opts)
-      local preinstalled = { "stylua", "shfmt", "shellcheck" }
+      local preinstalled = {
+        -- required by LazyVim default, but provided by system rather than Mason
+        "stylua",
+        "shfmt",
+
+        -- required by extra `lang.go`, but provided by system rather than Mason
+        "goimports", -- Go Package
+        -- "gofumpt",
+        "gomodifytags", -- Go Package
+        -- "impl",
+        "golangci-lint",
+        "delve", -- Go Package
+
+        -- required by extra `util.dot`, but provided by system rather than Mason
+        "shellcheck",
+      }
       local function missing(package)
         return not vim.tbl_contains(preinstalled, package)
       end
@@ -14,11 +29,15 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        -- required by extra `util.dot`, but provided by system rather than Mason
-        bashls = { mason = false },
+        -- required by extra `lang.go`, but provided by system rather than Mason
+        gopls = { mason = false },
+
         -- required by extra `lang.python`, but provided by system rather than Mason
         [vim.g.lazyvim_python_lsp or "pyright"] = { mason = false },
         [vim.g.lazyvim_python_ruff or "ruff"] = { mason = false },
+
+        -- required by extra `util.dot`, but provided by system rather than Mason
+        bashls = { mason = false },
       },
     },
   },
