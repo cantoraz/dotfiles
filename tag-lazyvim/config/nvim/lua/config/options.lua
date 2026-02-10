1547ef1de9bb87bc4ed3ae48959ea57d3670114a
Cantoraz Chou <cantoraz@gmail.com>
2026-02-10 17:30:25 +0800

    fix(lazyvim): use `winborder` option to set default border for all floating windows

--- tag-lazyvim/config/nvim/lua/config/options.lua
+++ tag-lazyvim/config/nvim/lua/config/options.lua
@@ -9,3 +9,4 @@ vim.g.lazyvim_python_lsp = "pyright"
 vim.g.lazyvim_python_ruff = "ruff"
 
 vim.o.colorcolumn = "72,80,100"
+vim.o.winborder = "rounded"
--- tag-lazyvim/config/nvim/lua/plugins/ui.lua
+++ tag-lazyvim/config/nvim/lua/plugins/ui.lua
@@ -1,48 +1,38 @@
 return {
   -- set the border of the float window
-  {
-    "saghen/blink.cmp",
-    opts = {
-      completion = {
-        menu = {
-          border = "rounded",
-        },
-        documentation = {
-          window = {
-            border = "rounded",
-          },
-        },
-      },
-      signature = {
-        window = {
-          border = "rounded",
-        },
-      },
-    },
-  },
-  {
-    "mason-org/mason.nvim",
-    opts = {
-      ui = {
-        border = "rounded",
-      },
-    },
-  },
+  -- {
+  --   "saghen/blink.cmp",
+  --   opts = {
+  --     completion = {
+  --       menu = { border = "rounded" },
+  --       documentation = { window = { border = "rounded" } },
+  --     },
+  --     signature = {
+  --       window = { border = "rounded" },
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     diagnostics = {
+  --       float = { border = "rounded" },
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "mason-org/mason.nvim",
+  --   opts = {
+  --     ui = {
+  --       border = "rounded",
+  --     },
+  --   },
+  -- },
   {
     "folke/noice.nvim",
     opts = {
       presets = {
         lsp_doc_border = true,
-      },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      diagnostics = {
-        float = {
-          border = "rounded",
-        },
       },
     },
   },
