ce2b188920360838aa5058401fd9d0cbcbe8164b
Cantoraz Chou <cantoraz@gmail.com>
2026-04-02 16:26:02 +0800

    feat(zsh): bind `Meta-Control-L` to clear screen and scrollback

--- tag-zsh/zshrc.d/70-key-bingings-clear-screen.zsh
+++ tag-zsh/zshrc.d/70-key-bingings-clear-screen.zsh
@@ -1,14 +1,12 @@
 # -*- mode: sh; sh-basic-offset: 2; -*-
 # vim: ft=zsh ts=2 sts=2 sw=2 et
 
-# Use Ctrl-l scroll the current screen contents into the scrollback
-# buffer and clear the screen.
-
 clear-only-screen() {
   printf "\e[H\e[2J"
 }
 
 clear-screen-and-scrollback() {
+  # NOTE: It works in bare shell, but not enough in tmux.
   printf "\e[H\e[3J"
 }
 
@@ -16,7 +14,10 @@ clear-screen-saving-contents-in-scrollback() {
   printf "\e[H\e[22J"
 }
 
-scroll-and-clear-screen() {
+# Use Control-L to clear the screen that scroll the current screen
+# contents into the scrollback buffer.
+
+clear-screen-into-scrollback() {
   # printf '\n%.0s' {1..$LINES}
   # zle clear-screen
   builtin print -rn -- $'\r\e[0J\e[H\e[22J' >"$TTY"
@@ -24,5 +25,19 @@ scroll-and-clear-screen() {
   builtin zle -R
 }
 
-zle -N scroll-and-clear-screen
-bindkey '^l' scroll-and-clear-screen
+zle -N clear-screen-into-scrollback
+bindkey '^L' clear-screen-into-scrollback
+
+# Use Meta-Control-L to clear the screen, and then the scrollback.
+
+clear-screen-then-scrollback() {
+  # NOTE: It's not enough that send "\e[H\e[3J", because tmux will save
+  # the current screen contents into the history buffer.
+  # So clear the screen first, and then clear the scrollback.
+  builtin print -rn -- $'\r\e[0J\e[H\e[2J\e[3J' >"$TTY"
+  builtin zle .reset-prompt
+  builtin zle -R
+}
+
+zle -N clear-screen-then-scrollback
+bindkey '^[^L' clear-screen-then-scrollback
